7a268c8f1480013e8738a61ed72baf3f554a4fc3
SWELLZ <microsoftdavid4@gmail.com>
2022-08-21 13:35:53 -0700

    add links to other pages

--- src/components/headerComponents/HeaderComponent.js
+++ src/components/headerComponents/HeaderComponent.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './HeaderComponent.css';
 import home from '../images/home.png';
 import tools from '../images/time.png';
 import journal from '../images/travel-journal.png';
+import user from '../images/user.png';
 
 function HeaderComponent() {
   return (
     <header>
         <nav id='nav-links'>
-            <img alt='home' src={home} className='icon' />
+            <Link to='/' ><img alt='home' src={home} className='icon' /></Link>
             <img alt='Journal' src={journal} className='icon' />
             <img alt='Tools' src={tools} className='icon' />
+            <Link to='sign-in' ><img alt='Sign In' src={user} className='icon' /></Link>
         </nav>
     </header>
   )
