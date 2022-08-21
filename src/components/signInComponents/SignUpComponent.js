6be796f62bbee888e9ed42133d9db0d004c5ed8a
SWELLZ <microsoftdavid4@gmail.com>
2022-08-21 16:46:15 -0700

    add firebase auth

--- src/components/signInComponents/SignUpComponent.js
+++ src/components/signInComponents/SignUpComponent.js
@@ -1,34 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 import './SignIn.css';
+import { auth } from '../../firebase-config';
 
 function SignUpComponent() {
+  const [registerUsername, setRegisterUsername] = useState('');
+  const [registerEmail, setRegisterEmail] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div className='sign-up-component'>
         <section id='sign-in-section'>
             <div className='sign-in-form'>
               <h1>Sign Up</h1>
-                <form>
+                <div className='form'>
                     <div className='input'>
                       <label htmlFor='username'>Username:</label>
-                      <input id='username' name='username' type='text' />
+                      <input 
+                        id='username' 
+                        name='username' 
+                        type='text'
+                        onChange={(e) => {setRegisterUsername(e.target.value)}} 
+                      />
                     </div>
                     <div className='input'>
                       <label htmlFor='email'>Email:</label>
-                      <input id='email' name='email' type='email' />
+                      <input 
+                        id='email' 
+                        name='email' 
+                        type='email'
+                        onChange={(e) => {setRegisterEmail(e.target.value)}} 
+                      />
                     </div>
                     <div className='input'>
                       <label htmlFor='password'>Password:</label>
-                      <input id='password' name='password' type='password' />
-                    </div>
-                    <div className='input'>
-                      <label htmlFor='confirm-password'>Confirm Password:</label>
-                      <input id='confirm-password' name='confirm-password' type='password' />
+                      <input 
+                        id='password' 
+                        name='password' 
+                        type='password' 
+                        onChange={(e) => {setRegisterPassword(e.target.value)}}
+                      />
                     </div>
                     <div className='sign-in-actions'>
-                      <button id='sign-in-button'>Sign In</button>
+                      <button id='sign-in-button' onClick={register}>Sign Up</button>
                       <a className='sign-up-text' href='/sign-in'>Already have an account? Sign In</a>
                     </div>
-                </form>
+                </div>
             </div>
         </section>
     </div>
