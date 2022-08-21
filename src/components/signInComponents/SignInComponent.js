1264b81a148ce441d88eb297f1d40decd0661346
Nehemiah Dias <microsoftdavid4@gmail.com>
2022-08-20 17:05:26 -0700

    style sign in page

--- src/components/signInComponents/SignInComponent.js
+++ src/components/signInComponents/SignInComponent.js
@@ -7,13 +7,22 @@ function SignInComponent() {
         <section id='sign-in-section'>
             <div className='sign-in-form'>
                 <form>
-                    <label htmlFor='username'>Username:</label>
-                    <input id='username' name='username' type='text' />
-                    <label htmlFor='email'>Email:</label>
-                    <input id='email' name='email' type='email' />
-                    <label htmlFor='password'>Password:</label>
-                    <input id='password' name='password' type='password' />
-                    <button>Sign In</button>
+                    <div className='input'>
+                      <label htmlFor='username'>Username:</label>
+                      <input id='username' name='username' type='text' />
+                    </div>
+                    <div className='input'>
+                      <label htmlFor='email'>Email:</label>
+                      <input id='email' name='email' type='email' />
+                    </div>
+                    <div className='input'>
+                      <label htmlFor='password'>Password:</label>
+                      <input id='password' name='password' type='password' />
+                    </div>
+                    <div className='sign-in-actions'>
+                      <button id='sign-in-button'>Sign In</button>
+                      <a className='sign-up-text' href='/'>Don't have an account? Sign Up</a>
+                    </div>
                 </form>
             </div>
         </section>
