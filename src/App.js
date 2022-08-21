6bf570866d023a0a0d8f49b8bb34ce72869d517f
Nehemiah Dias <microsoftdavid4@gmail.com>
2022-08-20 21:45:26 -0700

    add sign in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import HeaderComponent from "./components/headerComponents/HeaderComponent";
 import SignInComponent from "./components/signInComponents/SignInComponent";
+import SignUpComponent from "./components/signInComponents/SignUpComponent";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <SignInComponent />
-    //<HeaderComponent />
+    <BrowserRouter>
+      <Routes>
+        <Route path='sign-in' element={<SignInComponent />} />
+        <Route path='sign-up' element={<SignUpComponent/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/signInComponents/SignUpComponent.js
+++ src/components/signInComponents/SignUpComponent.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './SignIn.css';
 
-function SignInComponent() {
+function SignUpComponent() {
   return (
     <>
         <section id='sign-in-section'>
             <div className='sign-in-form'>
-              <h1>Sign In</h1>
+              <h1>Sign Up</h1>
                 <form>
+                    <div className='input'>
+                      <label htmlFor='username'>Username:</label>
+                      <input id='username' name='username' type='text' />
+                    </div>
                     <div className='input'>
                       <label htmlFor='email'>Email:</label>
                       <input id='email' name='email' type='email' />
@@ -16,9 +20,13 @@ function SignInComponent() {
                       <label htmlFor='password'>Password:</label>
                       <input id='password' name='password' type='password' />
                     </div>
+                    <div className='input'>
+                      <label htmlFor='confirm-password'>Confirm Password:</label>
+                      <input id='confirm-password' name='confirm-password' type='password' />
+                    </div>
                     <div className='sign-in-actions'>
                       <button id='sign-in-button'>Sign In</button>
-                      <a className='sign-up-text' href='/sign-up'>Don't have an account? Sign Up</a>
+                      <a className='sign-up-text' href='/sign-in'>Already have an account? Sign In</a>
                     </div>
                 </form>
             </div>
@@ -27,4 +35,4 @@ function SignInComponent() {
   )
 }
 
-export default SignInComponent
+export default SignUpComponent
