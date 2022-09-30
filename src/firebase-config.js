9a26561581b4364c637d2d2096e60292f293cb72
SWELLZ <microsoftdavid4@gmail.com>
2022-09-30 09:30:51 -0700

    fix api issue

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "lucid-dream-site.firebaseapp.com",
-    projectId: "lucid-dream-site",
-    storageBucket: "lucid-dream-site.appspot.com",
-    messagingSenderId: "794365686972",
-    appId: "1:794365686972:web:a594b36df15621d5bc4cb9",
-    measurementId: "G-49NB19LTQJ"
+    apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+    storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_APP_ID}`,
+    measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
   };
 
 const app = initializeApp(firebaseConfig);
