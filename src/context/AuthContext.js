58791ec7da71a4c2a880e74e3dac952db03a5be0
SWELLZ <microsoftdavid4@gmail.com>
2022-09-30 09:57:02 -0700

    remove clog

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -33,7 +33,6 @@ export const AuthContextProvider = ({children}) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            console.log(currentUser)
             setUser(currentUser)
         })
         return () => {
